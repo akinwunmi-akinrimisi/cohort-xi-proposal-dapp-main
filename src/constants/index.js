be9831439e0a31956501f4635de9efc80276a4be
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-10-03 14:45:25 +0100

    added: events and voting functionality

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const multicall2Address = "0x3c07F2cE2fB07EBB89C49b4657CC8B178A81d49E";
+export const proposalsContractAddress =
+  "0x72386218c2437e941140341B3774C5FB591F84eb";
--- src/hooks/useCreateProposal.js
+++ src/hooks/useCreateProposal.js
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { toast } from "react-toastify";
 import useContract from "./useContract";
 import { useAppKitAccount } from "@reown/appkit/react";
@@ -7,68 +7,76 @@ import { liskSepoliaNetwork } from "../connection";
 import { parseEther } from "ethers";
 
 const useCreateProposal = () => {
-    const contract = useContract(true);
-    const { address } = useAppKitAccount();
-    const { chainId } = useAppKitNetwork();
-    return useCallback(
-        async (description, recipient, amount, duration, minVote) => {
-            if (
-                !description ||
-                !recipient ||
-                !amount ||
-                !duration ||
-                !minVote
-            ) {
-                toast.error("Missing field(s)");
-                return;
-            }
-            if (!address) {
-                toast.error("Connect your wallet!");
-                return;
-            }
-            if (Number(chainId) !== liskSepoliaNetwork.chainId) {
-                toast.error("You are not connected to the right network");
-                return;
-            }
+  const contract = useContract(true);
+  const { address } = useAppKitAccount();
+  const { chainId } = useAppKitNetwork();
+  const [isLoading, setIsLoading] = useState(false);
 
-            if (!contract) {
-                toast.error("Cannot get contract!");
-                return;
-            }
+  return {
+    create: useCallback(
+      async (description, recipient, amount, duration, minVote) => {
+        if (!description || !recipient || !amount || !duration || !minVote) {
+          toast.error("Missing field(s)");
+          return;
+        }
 
-            try {
-                const estimatedGas = await contract.createProposal.estimateGas(
-                    description,
-                    recipient,
-                    parseEther(amount),
-                    duration,
-                    minVote
-                );
-                const tx = await contract.createProposal(
-                    description,
-                    recipient,
-                    parseEther(amount),
-                    duration,
-                    minVote,
-                    {
-                        gasLimit: (estimatedGas * BigInt(120)) / BigInt(100),
-                    }
-                );
-                const reciept = await tx.wait();
+        setIsLoading(true);
+        if (!address) {
+          toast.error("Connect your wallet!");
+          setIsLoading(false);
+          return;
+        }
+        if (Number(chainId) !== liskSepoliaNetwork.chainId) {
+          toast.error("You are not connected to the right network");
+          setIsLoading(false);
+          return;
+        }
 
-                if (reciept.status === 1) {
-                    toast.success("Proposal Creation successful");
-                    return;
-                }
-                toast.error("Proposal Creation failed");
-                return;
-            } catch (error) {
-                console.error("error while creating proposal: ", error);
-                toast.error("Proposal Creation errored");
+        if (!contract) {
+          toast.error("Cannot get contract!");
+          setIsLoading(false);
+          return;
+        }
+
+        try {
+          const estimatedGas = await contract.createProposal.estimateGas(
+            description,
+            recipient,
+            parseEther(amount),
+            duration,
+            minVote
+          );
+          const tx = await contract.createProposal(
+            description,
+            recipient,
+            parseEther(amount),
+            duration,
+            minVote,
+            {
+              gasLimit: (estimatedGas * BigInt(120)) / BigInt(100),
             }
-        },
-        [address, chainId, contract]
-    );
+          );
+          const reciept = await tx.wait();
+
+          if (reciept.status === 1) {
+            toast.success("Proposal Creation successful");
+            setIsLoading(false);
+            return;
+          }
+          toast.error("Proposal Creation failed");
+          setIsLoading(false);
+          return;
+        } catch (error) {
+          console.error("error while creating proposal: ", error);
+          toast.error("Proposal Creation errored");
+          setIsLoading(false);
+        }
+        return;
+      },
+      [address, chainId, contract]
+    ),
+    isLoading,
+  };
 };
 
 export default useCreateProposal;
